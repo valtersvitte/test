6df0e5bdede9b1071c0052e03cb9c49e5b5cd3f9
valtersvitte <vkvitte@gmail.com>
2019-02-28 16:14:16 +0200

    28.02.2019

--- masiv.js
+++ masiv.js
@@ -11,3 +11,23 @@ let cars = ["Saab", "Volvo", "BMW","Saab", "Volvo", "BMW"]
         for (i = 0; i < cars.length; i++)
         console.log((i+1) + ": " + cars[i])
         cars.forEach((el,index) => console.log('Masīva',index, 'elements: ',el))
+        if (cars instanceof Array) {
+            console.log('value is Array!')
+          } else{
+            console.log('value is no Array!')
+          }
+          if (cars="Volvo") {
+            console.log('value is Array!')
+          } else{
+            console.log('value is no Array!')
+          }
+         let indexVolvo=cars.indexOf("Volvo")
+         let indexVolvo2=cars.lastIndexOf("Volvo")
+          let cars2 = ["VW","Ford","Alfa"]
+          var c = cars.concat(cars2)
+          console.log(c)
+
+          let person = {Firtstname, Lastname, DateOfBirth, Gender, TripPlaces}
+          console.log(function a() {
+            return this.Firtstname + " " + this.Lastname + " " + this.DateOfBirth;
+          })
