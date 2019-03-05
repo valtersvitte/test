d3ce4d50e60c651ea4b2459f5395852db8b381a6
valtersvitte <vkvitte@gmail.com>
2019-03-05 12:05:55 +0200

    index

--- object.js
+++ object.js
@@ -1,4 +1,55 @@
-let person = {Firtstname, Lastname, DateOfBirth, Gender, TripPlaces}
-          console.log(function a() {
+let person = {Firtstname:'Kalvis',
+Lastname: 'Vitte', 
+DateOfBirth: '15,02,1995', 
+Gender: 'Male', 
+TripPlaces: ['Latvia'],
+fullName: function() {
             return this.Firtstname + " " + this.Lastname + " " + this.DateOfBirth;
-          })
+          },
+    changeName: function(name){
+        this.Firtstname=name;
+    },
+    addTrips: function(trip){
+        this.TripPlaces.push(trip)
+    },
+  
+};
+console.log(person.fullName());
+person.changeName('Walter');
+console.log(person.fullName());
+person.addTrips('German');
+console.log(person.TripPlaces);
+
+// tas pats tikai ar constructor
+
+class Construct{
+    constructor(Firtstname,Lastname,DateOfBirth,Gender,TripPlaces){
+        this.Firtstname = Firtstname;
+        this.Lastname = Lastname;
+        this.DateOfBirth = DateOfBirth; 
+        this.Gender = Gender; 
+        this.TripPlaces = TripPlaces || [];
+    }
+
+    get name(){
+        return this.Firtstname;
+    }
+ 
+
+
+}
+let person3 = new Construct('KALVIS');
+console.log(person3.name);
+// extend
+class Female extends Construct{
+    constructor(nameg){
+        super(nameg);
+        this.Gender='femele';
+    }
+    sayName() {
+        console.log('Sup! My name is ', this.Firtstname + '.');
+        super.sayHistory();
+      }
+}
+let girl = new Female('Elizabeth', 'Teilor', '02,02,2000');
+console.log(girl.sayName)
